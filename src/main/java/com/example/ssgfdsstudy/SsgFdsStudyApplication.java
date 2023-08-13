89a86b82edb0d5d24ec5702090d727df40620a89
pdh8788 <pdh1753@gmail.com>
2023-08-13 11:59:59 +0900

    Initial Commit

---
+++ src/main/java/com/example/ssgfdsstudy/SsgFdsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssgfdsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgFdsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsgFdsStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ssgfdsstudy/SsgFdsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssgfdsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgFdsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
